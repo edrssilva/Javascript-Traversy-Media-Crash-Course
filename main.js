0ecc70e85d0ca570469941f4f5e29a352de59a88
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-11 23:55:57 -0300

    Basic string methods and properties

--- main.js
+++ main.js
@@ -44,3 +44,41 @@ const oldConcatenation =
 const modernConcatenation = `My name is ${fullName} and I'm ${age} years old.`; // ES6 way 😎
 
 console.log(modernConcatenation);
+
+// ---------------  STRING METHODS   --------------- //
+
+// Properties don't need parenthesis, methods does.
+
+const helloWorld = "Hello World";
+
+// Get the length of the string
+const helloWorldLength = helloWorld.length;
+
+// Change cases
+const helloWorldCases = [
+  helloWorld.toUpperCase(),
+  helloWorld.toLowerCase(),
+  helloWorld.toLocaleUpperCase(),
+  helloWorld.toLocaleLowerCase(),
+];
+
+// Get a substring from the string - syntax: stringName.substring(int startIndex, int endAtIndex)
+const helloFromHelloWorld = helloWorld.substring(0, 5);
+
+// We can also use more than one method at a time
+const upperHelloFromHelloWorld = helloWorld.substring(0, 5).toUpperCase();
+
+// Split a string into an array
+const helloWorldArrayByLetter = helloWorld.split(""); // To split by letter we use a empty string as the method parameter
+const helloWorldArrayBySpace = helloWorld.split(" "); // To split by any other text, use the text as the method parameter
+
+console.log(
+  `
+  String length: ${helloWorldLength}
+  String cases: ${helloWorldCases}
+  Substring: ${helloFromHelloWorld}
+  Upper cased substring: ${upperHelloFromHelloWorld}
+  Splited into array by letter: ${helloWorldArrayByLetter}
+  Splited into array by space: ${helloWorldArrayBySpace}
+  `
+);
