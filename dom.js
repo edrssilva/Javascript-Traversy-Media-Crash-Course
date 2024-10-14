e7ce688026770d000bae347978ff33daaa626a02
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-14 17:58:22 -0300

    DOM Manipulation - Final APP
    Created an simple user register to a list using DOM manipulation.

--- dom.js
+++ dom.js
@@ -8,6 +8,39 @@
 // userList.children[2].innerText = "Brad";
 // userList.lastElementChild.innerHTML = "<h1>Title</h1>";
 // userList.style.background = "red";
+// userList.classList.add("bg-slate-700");
+// userList.classList.remove("bg-slate-700");
 
-const userList = document.querySelector("#users-list");
+const userForm = document.querySelector("#user-form");
+const usernameInput = document.querySelector("#username");
+const emailInput = document.querySelector("#email");
+const message = document.querySelector("#message");
 const submitButton = document.querySelector("#submit-button");
+const userList = document.querySelector("#users-list");
+
+userForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  console.log(usernameInput.value);
+  console.log(emailInput.value);
+
+  if (usernameInput.value.length == 0 || emailInput.value.length == 0) {
+    message.classList.remove("hidden");
+    message.classList.add("text-red-500");
+    message.innerHTML = "Please enter all fields";
+
+    setTimeout(() => message.classList.add("hidden"), 3000);
+  } else {
+    userList.classList.remove("hidden");
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(`${usernameInput.value} - ${emailInput.value}`)
+    );
+    userList.appendChild(li);
+
+    usernameInput.value = "";
+    emailInput.value = "";
+  }
+}
