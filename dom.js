aaa1ba702d733f31ce8563b916ccb3cebb3c3cf4
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-14 17:18:11 -0300

    DOM Selectors

--- dom.js
+++ dom.js
@@ -1,3 +1,13 @@
-// Single Element Selectors
-// DOCUMENT
-const userList = document.getElementById("users-list");
+//  --------------- ELEMENT SELECTORS --------------- //
+// DOCUMENT - GET ELEMENT BY CLASS NAME / TAG NAME - QUERY SELECTOR ALL (RECOMMENDED)
+// DOCUMENT - GET ELEMENT BY ID - QUERY SELECTOR (# . <>)
+
+// userList.remove();
+// userList.lastElementChild.remove();
+// userList.firstElementChild.remove();
+// userList.children[2].innerText = "Brad";
+// userList.lastElementChild.innerHTML = "<h1>Title</h1>";
+// userList.style.background = "red";
+
+const userList = document.querySelector("#users-list");
+const submitButton = document.querySelector("#submit-button");
